8c31759f2b35060a45202797aea48a82cc2aa91a
one-a <ihska16mry@gmail.com>
2016-01-20 08:09:48 +0900

    Add function

--- dev/app.js
+++ dev/app.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", function() {
   CSSDeviceManager.start();
-  document.getElementById("adc").addEventListener("change", function(e) {
-    console.log(e.target.value);
-  });
+  // document.getElementById("adc").addEventListener("change", function(e) {
+  //   console.log(e.target.value);
+  // });
 });
